884d58087891be7bd755bef68790c58f9b8a4fe6
Michal Tynior <michal.git@tynior.com>
2018-05-14 20:47:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Brain Archives Ghost theme
+
+### Install dependencies
+
+Run `yarn install` to install all required dependecies.
+
+### Build and minimize styles
+
+Run `gulp build_styles` to generate and minimize css styles from scss.
+
+### Development
+
+Run `gulp` to watch for changes and generate css from sass.
---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp             = require('gulp'),
+    sass             = require('gulp-sass'),
+    autoprefixer     = require('gulp-autoprefixer'),
+    sourcemaps       = require('gulp-sourcemaps'),
+    minifyCss        = require('gulp-clean-css'),
+    rename           = require('gulp-rename'),
+    gulpSequence     = require('gulp-sequence');
+
+
+gulp.task('sass', function(){
+    // sass directory
+    return gulp.src('./src/sass/**/*scss')
+            .pipe(sass())
+            //outputstyle (nested, compact, expanded, compressed)
+            .pipe(sass({outputStyle:'compact'}).on('error', sass.logError))
+            // sourcemaps
+            .pipe(sourcemaps.init())
+            // sourcemaps output directory
+            .pipe(sourcemaps.write(('./maps')))
+            // css output directory
+            .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('minify-css', function(){
+   return gulp.src('./assets/css/brainarchives.css')
+            .pipe(minifyCss())
+             // autoprefixer
+            .pipe(autoprefixer({
+                browsers: ['last 5 versions'],
+                cascade: false
+            }))
+            // minify css rename
+            .pipe(rename('brainarchives.min.css'))
+            // minify css output directory
+            .pipe(gulp.dest('./assets/css/'));
+});
+
+gulp.task('build_styles', gulpSequence('sass', 'minify-css'));
+
+gulp.task('watch_styles', function(){
+    gulp.watch('./src/sass/**/*.scss', ['build_styles']);
+});
+
+gulp.task('default', ['watch_styles']);
